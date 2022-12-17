30c57b52bb203718f24f659fffebc450545896d8
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-12-17 12:19:58 -0500

    made new changes

--- newFile.js
+++ newFile.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const newFile = () => {
   return (
+    <>
     <div>newFile</div>
+    <div>newFile</div>
+    <div>newFile</div>
+    <div>newFile</div>
+    </>
   )
 }
 
